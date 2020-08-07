4bf019e918a087b915f4b31e55dd482ea8b49ea9
Emanuel <emanuel.duarte.maia@gmail.com>
2020-08-07 17:23:19 -0300

    next level week dia 4: back-end 1/4

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
-require('express')()
+const express = require('express');
+const { request, response } = require('express');
+const server = express();
+
+server.use(express.static("public"))
 .get('/', (request, response)=>{
-  return response.send('Hi from NLW')
+  return response.sendFile(__dirname + "/views/index.html")
 })
 .get('/study', (request, response) =>{
-  return response.send('Página Study')
+  return response.sendFile(__dirname + "/views/study.html")
+})
+.get('/give-classes', (request, response) =>{
+  return response.sendFile(__dirname + "/views/give-classes.html")
 })
 .listen(5500);
-
